20f3bf19aa3a0344ae26cc47f03b0340561e568b
Benedikt Kaltenecker <benedikt@kaltenecker.org>
2022-08-22 18:06:19 +0200

    minor bugfixes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,19 @@
 import React from "react"
 
 export default function ItemList(props) {
+    const shortUrl = (url) => {
+        if (url) {
+            const array = url.split('/')
+            return `${array[0]}//${array[2]}`
+        }
+    }
+
     return (
         <div className="newsFeed">
             <div className="newsTitle">
                 <div className="numbers">{props.index}.</div>
-                <h4>{props.news.title}</h4>
+                <a href={props.news.url} target="_blank">{props.news.title}</a>
+                <div>{shortUrl(props.news.url)}</div>
             </div>
             <div className="newsInfo">
                 <div><p>{props.news.points} points by {props.news.author} | {props.news.num_comments} comments | ID: {props.news.objectID}</p></div>
